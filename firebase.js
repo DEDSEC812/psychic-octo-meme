702166e5cd840f0f9d2233897b7a49b2037192d2
8808 <odiuswadsonsayley@gmail.com>
2026-03-03 23:43:00 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,11 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
+// Konfig Firebase pou Infinity Chat
 const firebaseConfig = {
   apiKey: "AIzaSyC91wBXLyPhyhgAAlvTigqi1Y8QKiAlkDs",
   authDomain: "infinity-chat-a6cae.firebaseapp.com",
   projectId: "infinity-chat-a6cae",
-  storageBucket: "infinity-chat-a6cae.firebasestorage.app",
+  storageBucket: "infinity-chat-a6cae.appspot.com",
   messagingSenderId: "34564309390",
   appId: "1:34564309390:web:c0be111c957e61d267a4df",
   measurementId: "G-WH5XZ9J3P2"
